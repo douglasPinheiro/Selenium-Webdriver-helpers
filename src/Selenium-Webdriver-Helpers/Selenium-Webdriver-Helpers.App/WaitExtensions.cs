8e3f981427c19e980e194721871ceb0010680224
Douglas da Costa <douglaspinheiroti@gmail.com>
2015-09-15 09:46:06 -0300

    add method wait for ajax

--- src/Selenium-Webdriver-Helpers/Selenium-Webdriver-Helpers.App/WaitExtensions.cs
+++ src/Selenium-Webdriver-Helpers/Selenium-Webdriver-Helpers.App/WaitExtensions.cs
@@ -1,6 +1,19 @@
-﻿namespace Selenium_Webdriver_Helpers.App
+﻿using System.Threading;
+using OpenQA.Selenium;
+
+namespace Selenium_Webdriver_Helpers.App
 {
     public static class WaitExtensions
     {
+        public static void WaitForAjax(this IWebDriver driver)
+        {
+            while (true)
+            {
+                var ajaxIsComplete = (bool)(driver as IJavaScriptExecutor).ExecuteScript("return jQuery.active == 0");
+                if (ajaxIsComplete)
+                    break;
+                Thread.Sleep(100);
+            }
+        }
     }
 }
