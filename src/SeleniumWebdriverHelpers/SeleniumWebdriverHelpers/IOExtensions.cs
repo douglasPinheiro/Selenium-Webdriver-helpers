ec838e67b8d26246be77e9f2ef0200bcabaa3e98
Douglas da Costa <douglaspinheiroti@gmail.com>
2015-09-25 15:31:10 -0300

    Added method set,get and clear

---
+++ src/SeleniumWebdriverHelpers/SeleniumWebdriverHelpers/IOExtensions.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriverHelpers
+{
+    public static class IOExtensions
+    {
+        public static void SetText(this IWebElement element, string text)
+        {
+            element.SendKeys(text);
+        }
+
+        public static string GetText(this IWebElement element)
+        {
+            return element.Text;
+        }
+
+        public static IWebElement ClearText(this IWebElement element)
+        {
+            element.Clear();
+            return element;
+        }
+    }
+}
