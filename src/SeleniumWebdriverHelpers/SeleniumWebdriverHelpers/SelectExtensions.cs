a1b02f445a665dee3014b1cc6d97b998e0bf9d81
Douglas da Costa <douglaspinheiroti@gmail.com>
2015-09-25 15:08:04 -0300

    Add select methods

---
+++ src/SeleniumWebdriverHelpers/SeleniumWebdriverHelpers/SelectExtensions.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumWebdriverHelpers
+{
+    public static class SelectExtensions
+    {
+        public static IWebElement SelectElement(this IWebDriver browser, By by)
+        {
+            return browser.FindElement(by);
+        }
+
+        public static IEnumerable<IWebElement> SelectElements(this IWebDriver browser, By by)
+        {
+            return browser.FindElements(by);
+        }
+    }
+}
