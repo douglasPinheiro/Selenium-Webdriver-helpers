b4eebbd947ad9fa2a0621ecd9fd8125f98c7f9cf
Douglas da Costa <douglaspinheiroti@gmail.com>
2015-10-08 14:57:09 -0300

    ActorBase added in example test

---
+++ src/SeleniumWebdriverHelpers/SeleniumWebdriverHelpers.ExampleTest/Actors/Abstractions/ActorBase.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumWebdriverHelpers.ExampleTest.Actors.Abstractions
+{
+    public abstract class ActorBase
+    {
+        public string Url
+        {
+            get
+            {
+                return "http://address.com.br";
+            }
+        }
+
+        public static IWebDriver I;
+    }
+}
